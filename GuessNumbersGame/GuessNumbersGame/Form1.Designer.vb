018a7f03385e2edf66fae7eafc592afa19c4b9f6
Tanawat chinprapinporn <tanawatz@hotmail.com>
2018-12-11 15:26:53 +0700

    Completed

---
+++ GuessNumbersGame/GuessNumbersGame/Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ButtonStart = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LabelCount = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ButtonStart
+        '
+        Me.ButtonStart.Location = New System.Drawing.Point(32, 15)
+        Me.ButtonStart.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.ButtonStart.Name = "ButtonStart"
+        Me.ButtonStart.Size = New System.Drawing.Size(224, 53)
+        Me.ButtonStart.TabIndex = 0
+        Me.ButtonStart.Text = "เริ่มเกมใหม่"
+        Me.ButtonStart.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 90)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "ทายได้อีก"
+        '
+        'LabelCount
+        '
+        Me.LabelCount.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.LabelCount.Location = New System.Drawing.Point(101, 82)
+        Me.LabelCount.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LabelCount.Name = "LabelCount"
+        Me.LabelCount.Size = New System.Drawing.Size(84, 28)
+        Me.LabelCount.TabIndex = 2
+        Me.LabelCount.Text = "7"
+        Me.LabelCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(204, 89)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 17)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "ครั้ง"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(280, 140)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.LabelCount)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ButtonStart)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.Text = "GuessNumbers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonStart As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LabelCount As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ GuessNumbersGame/GuessNumbersGame/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Sub LabelCount_Click(sender As Object, e As EventArgs) Handles LabelCount.Click
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ButtonStart.Click
+        ButtonStart.Enabled = False
+        Dim RANDOM As Random = New Random
+        Dim GetNum As Integer = RANDOM.Next(1, 99)
+        Dim Tried As Integer = 7
+
+        For x = Tried - 1 To -1 Step -1
+
+            Dim GuessNum As Integer = InputBox(" กรุณาป้อนตัวเลขที่ต้องการทาย ")
+            If x = -1 Then
+                MessageBox.Show("จบเกมส์ คุุณแพ้แล้ว", " ", MessageBoxButtons.OK)
+            ElseIf GuessNum < GetNum Then
+                MessageBox.Show("มากกว่านี้", " ", MessageBoxButtons.OK)
+            ElseIf GuessNum > GetNum Then
+                MessageBox.Show("น้อยกว่านี้", " ", MessageBoxButtons.OK)
+            ElseIf GuessNum = GetNum Then
+                MessageBox.Show(GetNum & "....ถูกต้อง คุุณคือผู้ชนะ", " ", MessageBoxButtons.OK)
+                Exit For
+            Else
+                MessageBox.Show("กรุณาป้อนตัวเลข", " ", MessageBoxButtons.OK)
+                x += 1
+            End If
+            LabelCount.Text = x
+
+        Next
+        LabelCount.Text = 7
+        ButtonStart.Enabled = True
+    End Sub
+
+End Class
